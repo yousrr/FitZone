e09e34b95a2b9a6379690992c955611715a16e5a
yousrr <kingdome1020@gmail.com>
2026-01-16 16:22:45 +0100

    Delete unused file

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yourname.fitlife',
-  appName: 'FitLife',
-  webDir: 'dist'
-};
-
-export default config;
